40f1673a35b55d2233354397cc7637cd3e682ec0
Syed Jawad Ali <syedjawadali92@gmail.com>
2024-07-17 23:34:49 +0500

    Changes

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -56,27 +56,33 @@ const Contact = () => {
     }
     };
   return (
-    <div class='container' style={{ marginLeft: '200px' }}  >
-        <h2 class='mt-3'>Contact Us</h2>
-        <form action="action_page.php"  onSubmit={handleSubmit}>
 
-          <label for="name" class='mt-3'>Name</label>
-          <input type="text" id="name" name="name" placeholder="Your name.." value={name}
-            onChange= {e => setname(e.target.value)} />
-
-          <label for="country">Country</label>
-          <input type="text" id="country" name="country" placeholder="Your country name.."  value={country}
+    <div class="container contact-form" style={{ marginLeft: '200px' }}>
+    <div class="contact-image">
+      <img src="https://image.ibb.co/kUagtU/rocket_contact.png" alt="rocket_contact"/>
+    </div>
+    <form method="post" onSubmit={handleSubmit}>
+      <h3>Drop Us a Message</h3>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <input type="text" name="name" class="form-control" placeholder="Your Name *" value={name}
+                  onChange= {e => setname(e.target.value)} />
+          </div>
+          <div class="form-group">
+            <input type="text" name="name" class="form-control" placeholder="Your country *" value={country}
             onChange= {e => setcountry(e.target.value)} />
-
-          <label for="phone_number">Phone Number</label>
-          <input type="text" id="phone_number" name="phone_number" placeholder="Your phone number.."  value={phone_number}
-            onChange= {e => setphone_number(e.target.value)}/>
-
-          <label for="email">Email</label>
-          <input type="text" id="email" name="email" placeholder="Your email.." value={email}
-            onChange= {e => setemail(e.target.value)} />
-
-          <select class="form-select" aria-label="Default select example" onChange={e => setrestaurant(e.target.value)}>
+          </div>         
+          <div class="form-group">
+            <input type="text" name="phone" class="form-control" placeholder="Your Phone Number *" value={phone_number}
+                  onChange= {e => setphone_number(e.target.value)}/>
+          </div>
+          <div class="form-group">
+            <input type="email" name="email" class="form-control" placeholder="Your Email *"  value={email}
+                  onChange= {e => setemail(e.target.value)} />
+          </div>
+          <div class="form-group">
+          <select class="form-control" aria-label="Default select example" onChange={e => setrestaurant(e.target.value)}>
             <option selected>Select Restaurant</option>
 
               { restaurantRecords ?
@@ -87,23 +93,68 @@ const Contact = () => {
                 <option selected>No Records</option>
               }
           </select>
-
-          {/* <label for="country">Country</label>
-          <select id="country" name="country">
-            <option value="australia">Australia</option>
-            <option value="canada">Canada</option>
-            <option value="usa">USA</option>
-          </select> */}
-
-          <label for="subject">Subject</label>
-          <textarea id="subject" name="subject" placeholder="Write something.."  value={subject}
-            onChange= {e => setsubject(e.target.value)} style={ {height: '150px'} }></textarea>
-
-          {/* <input type="submit" value="Submit" /> */}
-          <button type="submit" class="btn btn-primary mt-3">Submit</button>
-
-        </form>
+          </div>
+          <div class="form-group">
+            <input type="submit" name="btnSubmit" class="btnContact" value="Send Message" />
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="form-group">
+            <textarea name="message" class="form-control" placeholder="Your Message *" style={{ width: '100%', height: '275px' }}
+            value={subject} onChange= {e => setsubject(e.target.value)} ></textarea>
+          </div>
+        </div>
       </div>
+    </form>
+  </div>
+
+
+
+
+
+    // <div class='container' style={{ marginLeft: '200px' }}  >
+    //     <h2 class='mt-3'>Contact Us</h2>
+    //     <form action="action_page.php"  onSubmit={handleSubmit}>
+
+    //       <label for="name" class='mt-3'>Name</label>
+    //       <input type="text" id="name" name="name" placeholder="Your name.." value={name}
+    //         onChange= {e => setname(e.target.value)} />
+
+    //       <label for="country">Country</label>
+    //       <input type="text" id="country" name="country" placeholder="Your country name.."  value={country}
+    //         onChange= {e => setcountry(e.target.value)} />
+
+    //       <label for="phone_number">Phone Number</label>
+    //       <input type="text" id="phone_number" name="phone_number" placeholder="Your phone number.."  value={phone_number}
+    //         onChange= {e => setphone_number(e.target.value)}/>
+
+    //       <label for="email">Email</label>
+    //       <input type="text" id="email" name="email" placeholder="Your email.." value={email}
+    //         onChange= {e => setemail(e.target.value)} />
+
+    //       <select class="form-select" aria-label="Default select example" onChange={e => setrestaurant(e.target.value)}>
+    //         <option selected>Select Restaurant</option>
+
+    //           { restaurantRecords ?
+    //             restaurantRecords.map(item => (
+    //               <option value={item.id} key={item.id}>{item.name}</option>
+    //             ))
+    //             :
+    //             <option selected>No Records</option>
+    //           }
+    //       </select>
+
+    //       <label for="subject">Subject</label>
+    //       <textarea id="subject" name="subject" placeholder="Write something.."  value={subject}
+    //         onChange= {e => setsubject(e.target.value)} style={ {height: '150px'} }></textarea>
+
+    //       {/* <input type="submit" value="Submit" /> */}
+    //       <button type="submit" class="btn btn-primary mt-3">Submit</button>
+
+    //     </form>
+    //   </div>4
+
+    //
   )
 }
 
@@ -111,35 +162,3 @@ export default Contact
 
 
 
-{/* <div class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header text-center">
-                        <h3>Contact Us</h3>
-                    </div>
-                    <div class="card-body">
-                        <form>
-                            <div class="form-group">
-                                <label for="name">Name</label>
-                                <input type="text" class="form-control" id="name" placeholder="Your Name"/>
-                            </div>
-                            <div class="form-group">
-                                <label for="email">Email</label>
-                                <input type="email" class="form-control" id="email" placeholder="Your Email"/>
-                            </div>
-                            <div class="form-group">
-                                <label for="phone">Phone</label>
-                                <input type="text" class="form-control" id="phone" placeholder="Your Phone Number"/>
-                            </div>
-                            <div class="form-group">
-                                <label for="message">Message</label>
-                                <textarea class="form-control" id="message" rows="5" placeholder="Your Message"></textarea>
-                            </div>
-                            <button type="submit" class="btn btn-primary btn-block">Send Message</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> */}
